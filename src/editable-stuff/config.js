ee5be9be4503a99e6e61e955b2e7f312c4de6593
sanjid salam <sanjids@active.asianetindia.com>
2021-05-28 20:40:44 +0530

    final

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -8,28 +8,28 @@ const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
   firstName: "Sanjid",
   middleName: "",
-  lastName: "salam",
-  message: " testing. ",
+  lastName: "Salam",
+  message: " Cloud Engineer | AWS | GCP | Azure | Linux | Windows | Docker | Git | Bash ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/getsan4u",
     },
     {
       image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://www.facebook.com/getsan4u",
     },
     {
       image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
+      url: "https://www.instagram.com/getsan4u/",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/getsan4u/",
     },
     {
       image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
+      url: "https://www.twitter.com/getsan4u/",
     },
   ],
 };
@@ -41,7 +41,7 @@ const mainBody = {
 //b) a link to an hosted image
 //      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
 //c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
+//      i.e: profilePictureLink: require("../editable-stuff/getsan4u.png"),
 //d) If you do not want any picture to be displayed, just leave it empty :)
 //      i.e: profilePictureLink: "",
 // For Resume either provide link to your resume or import from "editable-stuff" directory
@@ -51,10 +51,10 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/sanjid.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
+    "My name is Sanjid Salam.I’m a graduate from M E S College Of Engineering affiliated to Calicut University with a B.Tech degree in Computer Engineering. I'm most passionate about learning new things, and my goal is to pursue this passion within the field of devops engineering. In my free time I used to learn about devops tools and open source projects.",
   resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 };
 
@@ -66,8 +66,8 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
+  gitHubUsername: "getsan4u", //i.e."johnDoe12Gh"
+  reposLength: 2,
   specificRepos: [],
 };
 
@@ -79,12 +79,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     {
-      img: require("../editable-stuff/hashirshoaeb.png"),
+      img: require("../editable-stuff/sanjid.png"),
       label: "First slide label",
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
     },
     {
-      img: require("../editable-stuff/hashirshoaeb.png"),
+      img: require("../editable-stuff/sanjid.png"),
       label: "Second slide label",
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
     },
@@ -100,14 +100,21 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "Linux", value: 80 },
+    { name: "Windows", value: 70 },
+    { name: "AWS", value: 65 },
+    { name: "Azure", value: 60},
+    { name: "GCP", value: 60},
+    { name: "Apache", value: 70},
+    { name: "Nginx", value: 70},
+    { name: "Docker", value: 75},
+    { name: "Kubernetes", value: 65},
+    { name: "Git", value: 70},
+    { name: "SQL/NoSQL", value: 65},
+    { name: "C/C++", value: 70 },
+    { name: "JavaScript", value: 70 },
+    { name: "Python", value: 70 },
+
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
@@ -126,8 +133,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for full-time DevOps Engineer or Cloud Engineer opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "sanjid.salam.p@gmail.com",
 };
 
 const experiences = {
@@ -135,14 +142,14 @@ const experiences = {
   heading: "Experiences",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      role: 'System Engineer at Activelobby Information System Pvt Ltd, Kochi.',// Here Add Company Name
+      companylogo: require('../assets/img/active.png'),
+      date: 'May 2019 – Present',
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      role: 'System Administrator (Trainee) at Spectrum Soft Tech Solution Pvt Ltd, Kochi',
+      companylogo: require('../assets/img/spectrum.png'),
+      date: 'October 2018 – March 2019',
     },
   ]
 }
